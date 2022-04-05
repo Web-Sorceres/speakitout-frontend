b3d3c830d79e2ed75c0d9e92f0afa739b04936cb
Sahil Khadka <sahilkhadka767@gmail.com>
2022-04-05 16:22:09 +0545

    small changes

--- src/components/LandingNavbar.js
+++ src/components/LandingNavbar.js
@@ -1,4 +1,4 @@
-import {React, useEffect, useState} from "react";
+import {React, useState} from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
 
