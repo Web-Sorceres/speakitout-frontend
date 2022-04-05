30c2bb06a22399196e7ef9a8977a467bb9993683
Abid Adhikari <abidadhikari@gmail.com>
2022-04-05 13:28:48 +0545

    creating new branch

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,6 +4,7 @@ function ErrorPage(props) {
     return (
         <div>
             <h1>SALA BOKA JUN MAN LAGYO TEI PAGE MA JANXAS. LAAJ SARAM KEI XAINA HUH.</h1>
+            <h2>Oh my God</h2>
         </div>
     );
 }
